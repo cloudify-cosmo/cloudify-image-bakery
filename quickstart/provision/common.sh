bc385727586d35fdc402743fc553a2c4badb300c
Dan Kilman <dankilman@gmail.com>
2015-11-03 10:01:57 +0200

    update explicit path to id_rsa

--- quickstart/provision/common.sh
+++ quickstart/provision/common.sh
@@ -96,11 +96,11 @@ function create_blueprints_and_inputs_dir
 
 function configure_nodecellar_blueprint_inputs
 {
-echo '
+echo "
 host_ip: 10.10.1.10
 agent_user: vagrant
-agent_private_key_path: /root/.ssh/id_rsa
-' >> ~/cloudify/blueprints/inputs/nodecellar-singlehost.yaml
+agent_private_key_path: /home/${USERNAME}/.ssh/id_rsa
+" >> ~/cloudify/blueprints/inputs/nodecellar-singlehost.yaml
 }
 
 function configure_shell_login
