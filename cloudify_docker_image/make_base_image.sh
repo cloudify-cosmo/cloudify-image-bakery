9f6d86fd96e1ac7029c14e7a18e150ec16a6656a
opencm <opencm@gigaspaces.com>
2018-03-12 16:18:34 +0100

    Creating community and premium docker image, uploading the images to cloudifyplatform organization  (#42)

--- cloudify_docker_image/make_base_image.sh
+++ cloudify_docker_image/make_base_image.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
-CONTAINER_NAME="cfy_manager_base"
-BASE_IMAGE="centos7_base_image"
+CONTAINER_NAME="cfy-manager-base"
+BASE_IMAGE="latest-centos7-base-image"
 DOCKER_RUN_FLAGS="--name ${CONTAINER_NAME} -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /run
  --tmpfs /run/lock --security-opt seccomp:unconfined --cap-add SYS_ADMIN"
-declare -a IMAGE_TAGS=( "latest_centos_base_image" "centos7_v1.0" )
+declare -a IMAGE_TAGS=( "latest-centos7-base-image" "centos7-v1.2" )
 DOCKER_REPO="community"
 
 set +u
-if [ -z "$DOCKER_ID_USER" ] || [ -z "$DOCKER_ID_PASSWORD" ];
+if [ -z "$DOCKER_BUILD_ID" ] || [ -z "$DOCKER_BUILD_PASSWORD" ] || [ -z "$DOCKER_ORGANIZATION" ];
   then
     echo "Docker username and password must be set."
     exit
@@ -31,10 +31,10 @@ echo "Removing the used container..."
 docker rm $CONTAINER_NAME
 
 echo "Uploading the image..."
-docker login -u="${DOCKER_ID_USER}" -p="${DOCKER_ID_PASSWORD}"
+docker login -u="${DOCKER_BUILD_ID}" -p="${DOCKER_BUILD_PASSWORD}"
 for i in "${IMAGE_TAGS[@]}"
 do
-	docker tag $BASE_IMAGE ${DOCKER_ID_USER}/${DOCKER_REPO}:$i
+	docker tag $BASE_IMAGE ${DOCKER_ORGANIZATION}/${DOCKER_REPO}:$i
 done
-docker push ${DOCKER_ID_USER}/${DOCKER_REPO}
+docker push ${DOCKER_ORGANIZATION}/${DOCKER_REPO}
 docker logout
--- cloudify_docker_image/make_cfy_manager_image.sh
+++ cloudify_docker_image/make_cfy_manager_image.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 set -eu
+
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied. Please supply the CFY RPM URL as the first argument."
@@ -8,23 +10,43 @@ fi
 # RPM URL must be supplied as the first argument
 CFY_RPM_URL=$1
 CFY_RPM="cloudify-manager-install.rpm"
-CONTAINER_NAME="cfy_manager"
-BASE_IMAGE="centos7_base_image"
-IMAGE_PUB_NAME="docker_cfy_manager"
-declare -a IMAGE_TAGS=( "latest_centos_manager" "centos7_v1.0_manager_v4.3" )
+CONTAINER_NAME="cfy-manager"
+BASE_IMAGE="$(docker images | grep latest-centos7-base-image | awk '{print $3}')"
+IMAGE_PUB_NAME="docker-cfy-manager"
+declare -a IMAGE_TAGS=( "latest" "cloudify-manager-$VERSION-$PRERELEASE" )
 DOCKER_RUN_FLAGS="--name ${CONTAINER_NAME} -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /run
  --tmpfs /run/lock --security-opt seccomp:unconfined --cap-add SYS_ADMIN"
 MANAGER_CONFIG_LOCATION="/etc/cloudify"
-DOCKER_REPO="premium"
 
-set +u
-if [ -z "$DOCKER_ID_USER" ] || [ -z "$DOCKER_ID_PASSWORD" ];
-  then
-    echo "Docker username and password must be set."
-    exit
-fi
-set -u
+function upload_image_to_registry
+{
+	docker login -u="${DOCKER_BUILD_ID}" -p="${DOCKER_BUILD_PASSWORD}"
+	for i in "${IMAGE_TAGS[@]}"
+	do
+		docker tag $IMAGE_PUB_NAME ${DOCKER_ORGANIZATION}/${DOCKER_REPO}:$i
+		set +e
+		echo "Removing the ${DOCKER_ORGANIZATION}/${DOCKER_REPO}:$i image from registry"
+		TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_ID_USER}'", "password": "'${DOCKER_ID_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+		curl -X DELETE  -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DOCKER_ORGANIZATION}/${DOCKER_REPO}/tags/${i}/
+		set -e
+		echo "Uploading the ${DOCKER_ORGANIZATION}/${DOCKER_REPO}:$i image"
+		docker push ${DOCKER_ORGANIZATION}/${DOCKER_REPO}:$i
+	done
 
+	docker logout
+}
+function get_repo
+{
+	if [[ $CFY_RPM_URL =~ "community" ]];then
+	    DOCKER_REPO="community"
+	else
+	    DOCKER_REPO="premium"
+	fi
+}
+
+get_repo
+
+echo "Creating container..."
 docker run ${DOCKER_RUN_FLAGS} ${BASE_IMAGE}
 CONTAINER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
 
@@ -53,11 +75,3 @@ docker stop $CONTAINER_NAME
 echo "Removing the used container..."
 docker rm $CONTAINER_NAME
 
-echo "Uploading the image..."
-docker login -u="${DOCKER_ID_USER}" -p="${DOCKER_ID_PASSWORD}"
-for i in "${IMAGE_TAGS[@]}"
-do
-	docker tag $IMAGE_PUB_NAME ${DOCKER_ID_USER}/${DOCKER_REPO}:$i
-done
-docker push ${DOCKER_ID_USER}/${DOCKER_REPO}
-docker logout
